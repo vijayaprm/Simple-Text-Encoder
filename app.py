92f3646844ab041a3c0ba274618bdd1f5ff83323
Spiritedswordsman <amanchandrakumar202@gmail.com>
2023-04-24 15:23:31 +0530

    Passed initial unit test case

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ class TestEncryption(unittest.TestCase):
     def test_inputExists(self):
         self.assertIsNotNone(self.mymsg)        #mymsg means mymessage
 
-
+     def setUp(self):
+        self.mymsg = 1                          # assigned mymsg to pass the first unit
+        
 if __name__ == "__main__":
     unittest.main()
