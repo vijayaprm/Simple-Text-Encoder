5b9f5bef79020a7c419ba315462a3a0f5e50f54e
Spiritedswordsman <amanchandrakumar202@gmail.com>
2023-04-25 21:30:06 +0530

    Test case for checking if mymsg is of type string

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import unittest
 
 class TestEncryption(unittest.TestCase):
+    
+    def setUp(self):                            
+        self.mymsg = 1                      # assigned mymsg to pass the first unit
+    
     #we will do all the tests here
+    #Test 1 checking if value is assignmed to the argument mymsg 
     def test_inputExists(self):
         self.assertIsNotNone(self.mymsg)        #mymsg means mymessage
 
-     def setUp(self):
-        self.mymsg = 1                          # assigned mymsg to pass the first unit
+    #Test 2 to confirm if mymsg is of type str
+    def test_inputType(self):
+        self.assertIsInstance(self.mymsg, str)
         
+
 if __name__ == "__main__":
     unittest.main()
